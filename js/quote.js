00927c31c268744c948cc9e65016210639865911
s0ojin <onepst@hanyang.ac.kr>
2022-03-20 22:32:11 +0900

    first commit

---
+++ js/quote.js
@@ -0,0 +1,46 @@
+const quotes = [
+    {
+        quote: "One must desire something to be alive.",
+        author: "Margaret Deland",
+    },
+    {
+        quote: "You need chaos in your soul to give birth to a dancing star.",
+        author: "Friedrich Nietsche",
+    },
+    {
+        quote: "Kites rise highest against the wind - not with it.",
+        author: "Sir Winston Churchill",
+    },
+    {
+        quote: "Strong reasons make strong actions.",
+        author: "William Shakespeare",
+    },
+    {
+        quote: "If you don't risk anything you risk even more.",
+        author: "Erica Jong",
+    },
+    {
+        quote: "The journey is the reward.",
+        author: "Steve Jobs",
+    },
+    {
+        quote: "There is no great genius without some touch of madness.",
+        author: "Seneca",
+    },
+    {
+        quote: "Respect yourself and others will respect you.",
+        author: "Confucius"
+    },
+    {
+        quote: "Genius is nothing but a great capacity for patience.",
+        author: "Buffon",
+    }
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "9fc16af12f3217a3f63a2ddf3ef3c229";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then((response) => response.json()).then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} ${Math.round(data.main.temp)}º`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you, No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
