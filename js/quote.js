76f1951f65f4207cb335ec6a42fc1efc8e5cb1f3
s0ojin <onepst@hanyang.ac.kr>
2022-03-20 22:42:01 +0900

    test2

--- js/quote.js
+++ js/quote.js
@@ -43,4 +43,4 @@ const author = document.querySelector("#quote span:last-child");
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;aaaa
+author.innerText = todaysQuote.author;
